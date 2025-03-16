468e3030f5fa8b67c978c6ca283ccb9aabcb0f3a
uunniii <1964599174@qq.com>
2025-03-16 11:38:51 +0800

    Update friends.tsx
    111

--- data/friends.tsx
+++ data/friends.tsx
@@ -39,6 +39,14 @@ export const Friends: Friend[] = [
         avatar: 'https://avatars.githubusercontent.com/u/59783101?v=4',
         tips: "友链具有自反性~"
     }
+     {
+        title: 'JL LIU',
+        cname: "uni",
+        description: 'Auto.Major,SISE,NEU',
+        website: 'https://uunniii.space',
+        avatar: 'https://avatars.githubusercontent.com/u/182308101?v=4',
+        tips: "These fleeting days are all we call youth......"
+    }
 ]
 
 export type Friend = {
